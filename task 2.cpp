d98997f33eb1a9f67dfd49d19f0a7d4070f0d000
danishsakandergithub <149076153+danishsakandergithub@users.noreply.github.com>
2023-10-26 19:13:20 +0500

    Add files via upload

---
+++ task 2.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std ;
+int main (){
+	cout << " First Name\tLast name\tDegree\t\tRollno\t\tBatch "<<endl ;
+	cout <<" Ali\t\tMunir\t\tDS\t\t001\t\tF22" <<endl ;
+	cout <<" Ayesha\t\tRazzaq\t\tDS\t\t002\t\tF22"<<endl ;
+	return 0 ;
+}
